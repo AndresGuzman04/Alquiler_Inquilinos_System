1551f1f00919922cb08b366be884675f74afbfd4
Denis Vasquez <clasic.exper2023@gmail.com>
2023-11-08 11:08:18 -0600

    Modificando Form login

--- PresentatorLayer/Forms/Login.Designer.cs
+++ PresentatorLayer/Forms/Login.Designer.cs
@@ -28,19 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            titulolabel = new Label();
+            usernametextBox = new TextBox();
+            passwordlabel = new Label();
+            usernamelabel = new Label();
+            passwordtextBox = new TextBox();
+            loginbutton = new Button();
             SuspendLayout();
+            // 
+            // titulolabel
+            // 
+            titulolabel.AutoSize = true;
+            titulolabel.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            titulolabel.ForeColor = SystemColors.InfoText;
+            titulolabel.Location = new Point(178, 20);
+            titulolabel.Name = "titulolabel";
+            titulolabel.Size = new Size(83, 31);
+            titulolabel.TabIndex = 0;
+            titulolabel.Text = "LOGIN";
+            // 
+            // usernametextBox
+            // 
+            usernametextBox.Location = new Point(136, 88);
+            usernametextBox.Name = "usernametextBox";
+            usernametextBox.Size = new Size(227, 27);
+            usernametextBox.TabIndex = 1;
+            // 
+            // passwordlabel
+            // 
+            passwordlabel.AutoSize = true;
+            passwordlabel.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            passwordlabel.ForeColor = SystemColors.InfoText;
+            passwordlabel.Location = new Point(12, 156);
+            passwordlabel.Name = "passwordlabel";
+            passwordlabel.Size = new Size(101, 28);
+            passwordlabel.TabIndex = 2;
+            passwordlabel.Text = "Password";
+            // 
+            // usernamelabel
+            // 
+            usernamelabel.AutoSize = true;
+            usernamelabel.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            usernamelabel.ForeColor = SystemColors.InfoText;
+            usernamelabel.Location = new Point(12, 84);
+            usernamelabel.Name = "usernamelabel";
+            usernamelabel.Size = new Size(106, 28);
+            usernamelabel.TabIndex = 3;
+            usernamelabel.Text = "Username";
+            // 
+            // passwordtextBox
+            // 
+            passwordtextBox.Location = new Point(136, 156);
+            passwordtextBox.Name = "passwordtextBox";
+            passwordtextBox.Size = new Size(227, 27);
+            passwordtextBox.TabIndex = 4;
+            // 
+            // loginbutton
+            // 
+            loginbutton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            loginbutton.ForeColor = SystemColors.InfoText;
+            loginbutton.Location = new Point(178, 268);
+            loginbutton.Name = "loginbutton";
+            loginbutton.Size = new Size(100, 43);
+            loginbutton.TabIndex = 5;
+            loginbutton.Text = "Enter";
+            loginbutton.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(419, 360);
+            Controls.Add(loginbutton);
+            Controls.Add(passwordtextBox);
+            Controls.Add(usernamelabel);
+            Controls.Add(passwordlabel);
+            Controls.Add(usernametextBox);
+            Controls.Add(titulolabel);
+            ForeColor = SystemColors.Control;
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Login";
             Text = "Login";
             Load += Login_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label titulolabel;
+        private TextBox usernametextBox;
+        private Label passwordlabel;
+        private Label usernamelabel;
+        private TextBox passwordtextBox;
+        private Button loginbutton;
     }
 }
