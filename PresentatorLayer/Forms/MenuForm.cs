b78e2733f5f71c591daa260dee47c701c9e23d4b
Denis Vasquez <clasic.exper2023@gmail.com>
2023-11-23 21:50:16 -0600

    Creando codigo para abrir forms en el panel

--- PresentatorLayer/Forms/MenuForm.cs
+++ PresentatorLayer/Forms/MenuForm.cs
@@ -21,78 +21,45 @@ namespace PresentatorLayer.Forms
         {
             Application.Exit();
         }
-        private void OpenInquilinnosForm(Form inquilinoForm)
-        {
-            inquilinoForm.TopLevel = false;
-            inquilinoForm.Dock = DockStyle.Fill;
-            contenidopanel.Controls.Add(inquilinoForm);
-            contenidopanel.Tag = inquilinoForm;
-            inquilinoForm.BringToFront();
-            inquilinoForm.Show();
-        }
+        private Form ActiveForm = null;
 
+        private void OpenForms(Form forms)
+        {
+            if(ActiveForm != null)
+            {
+                ActiveForm.Close();
+            }
+            forms.TopLevel = false;
+            forms.Dock = DockStyle.Fill;
+            contenidopanel.Controls.Add(forms);
+            contenidopanel.Tag = forms;
+            forms.BringToFront();
+            forms.Show();
+        }
+        
         private void inquilinosmenubutton_Click_1(object sender, EventArgs e)
         {
-            //OpenInquilinnosForm(new inquilinosForm());
+            OpenForms(new inquilinosForm());
         }
 
-
-        private void OpenPropiedadesForm(Form PropiedadesForm)
-        {
-            PropiedadesForm.TopLevel = false;
-            PropiedadesForm.Dock = DockStyle.Fill;
-            contenidopanel.Controls.Add(PropiedadesForm);
-            contenidopanel.Tag = PropiedadesForm;
-            PropiedadesForm.BringToFront();
-            PropiedadesForm.Show();
-        }
         private void propiedadmenubutton_Click(object sender, EventArgs e)
         {
-            //OpenPropiedadesForm(new PropiedadesForm());
+            OpenForms(new PropiedadesForm());
         }
-        private void OpenContratosForm(Form ContratosForm)
-        {
-            ContratosForm.TopLevel = false;
-            ContratosForm.Dock = DockStyle.Fill;
-            contenidopanel.Controls.Add(ContratosForm);
-            contenidopanel.Tag = ContratosForm;
-            ContratosForm.BringToFront();
-            ContratosForm.Show();
-
-        }
+        
         private void contratosmenubutton_Click(object sender, EventArgs e)
         {
-            //OpenContratosForm(new ContratosForm());
+            OpenForms(new ContratosForm());
         }
-        private void OpenMantenimientoForm(Form MantenimientoForm)
-        {
-            MantenimientoForm.TopLevel = false;
-            MantenimientoForm.Dock = DockStyle.Fill;
-            contenidopanel.Controls.Add(MantenimientoForm);
-            contenidopanel.Tag = MantenimientoForm;
-            MantenimientoForm.BringToFront();
-            MantenimientoForm.Show();
-
-        }
-
+        
         private void mantenimientomenubutton_Click(object sender, EventArgs e)
         {
-            //OpenMantenimientoForm(new MantenimientoForm());
-        }
-
-        private void OpenPagosForm(Form PagosForm)
-        {
-            PagosForm.TopLevel = false;
-            PagosForm.Dock= DockStyle.Fill;
-            contenidopanel.Controls.Add(PagosForm);
-            contenidopanel.Tag = PagosForm; 
-            PagosForm.BringToFront();
-            PagosForm.Show();
+            OpenForms(new MantenimientoForm());
         }
 
         private void pagosmenubutton_Click(object sender, EventArgs e)
         {
-            OpenPagosForm(new PagosForm());
+            OpenForms(new PagosForm());
         }
     }
 }
