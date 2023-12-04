689549ec13ef38fcd1acc4ebc09dc6b4a4ded50f
Andres Javier Guzman Lovo <124549634+AndresGuzman04@users.noreply.github.com>
2023-12-04 09:56:45 -0600

    hh

--- PresentatorLayer/Forms/Register.cs
+++ PresentatorLayer/Forms/Register.cs
@@ -55,7 +55,9 @@ namespace PresentatorLayer.Forms
                 MessageBox.Show($"Error durante el registro: {ex.Message}");
             }
 
-           
+            var mail = new SendMailService();
+            string body = "Bienvenido" + emailRegister.Text + "\n Gracias por preferirnos";
+            mail.SendMail("Bienvenido a alquiler Cheros", body, emailRegister.Text, "Cliente");
         }
 
         private void ShowValidationErrors(List<string> errors)
